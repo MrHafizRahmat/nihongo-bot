307dcd13ecf995d3450b4e6a12b44227852e4c76
HafizRahmatUTM <hafizuddin2001@graduate.utm.my>
2026-06-12 22:01:28 +0800

    trying something please fix

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -676,7 +676,7 @@ export default function ChatPage() {
           ))}
         </div>
 
-        {!sessionId ? (
+        {!sessionActive ? (
           <StartScreen lessonMode={lessonMode} onStart={handleStart} starting={starting} />
         ) : (
           <>
